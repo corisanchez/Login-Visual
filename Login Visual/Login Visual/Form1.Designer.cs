00620ca45f8783fb018fb83bffd3aa78d102db0e
Corina Sanchez Jimenez <csanchez41@ucol.mx>
2019-08-22 20:34:08 -0500

    Confirmo

---
+++ Login Visual/Login Visual/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Login_Visual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contra = new System.Windows.Forms.TextBox();
+            this.salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // iniciar
+            // 
+            this.iniciar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.iniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iniciar.Location = new System.Drawing.Point(240, 523);
+            this.iniciar.Name = "iniciar";
+            this.iniciar.Size = new System.Drawing.Size(147, 50);
+            this.iniciar.TabIndex = 0;
+            this.iniciar.Text = "INICIAR";
+            this.iniciar.UseVisualStyleBackColor = false;
+            this.iniciar.Click += new System.EventHandler(this.Iniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1030, 71);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SISTEMA ULTRA MEGA SUPER HIPER SECRETO";
+            // 
+            // usuario
+            // 
+            this.usuario.Location = new System.Drawing.Point(355, 223);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(524, 22);
+            this.usuario.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(134, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "USUARIO:";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(134, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CONTRASEÑA:";
+            // 
+            // contra
+            // 
+            this.contra.Location = new System.Drawing.Point(355, 386);
+            this.contra.Name = "contra";
+            this.contra.PasswordChar = '*';
+            this.contra.Size = new System.Drawing.Size(524, 22);
+            this.contra.TabIndex = 5;
+            // 
+            // salir
+            // 
+            this.salir.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salir.Location = new System.Drawing.Point(707, 523);
+            this.salir.Name = "salir";
+            this.salir.Size = new System.Drawing.Size(140, 50);
+            this.salir.TabIndex = 6;
+            this.salir.Text = "SALIR";
+            this.salir.UseVisualStyleBackColor = false;
+            this.salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1116, 671);
+            this.Controls.Add(this.salir);
+            this.Controls.Add(this.contra);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usuario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button iniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox contra;
+        private System.Windows.Forms.Button salir;
+    }
+}
+
---
+++ Login Visual/Login Visual/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Visual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Iniciar_Click(object sender, EventArgs e)
+        {
+            if (usuario.Text == "juancho" & contra.Text == "123tamarindo")
+            {
+                MessageBox.Show("Ta' bien, si eres.");
+            }
+            else
+            {
+                MessageBox.Show("Sacate de aqui, no eres.");
+            }
+        }
+    }
+}
